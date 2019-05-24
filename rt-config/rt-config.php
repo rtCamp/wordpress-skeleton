bf320a809f5c666eb9b390c503003ccb1387f0da
Chandra Patel <chandra.ddu@gmail.com>
2019-05-24 14:42:03 +0530

    Add rt-config file to define additional constants

---
+++ rt-config/rt-config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Rtmedia config file.
+ *
+ * This file can be used similarly we are using wp-config.php or vip-config.php file
+ * Example: We can for defining constant Keys for the API, Plugins etc. in this file.
+ *
+ * This file is loaded very early (immediately after `wp-config.php`), which means that most WordPress APIs,
+ * classes, and functions are not available. The code below should be limited to pure PHP.
+ *
+ * @package wordpress-skeleton
+ */
+
+// Disable updates for theme and plugin.
+define( 'DISALLOW_FILE_MODS', true );
