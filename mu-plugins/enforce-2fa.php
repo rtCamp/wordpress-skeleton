938b319214fe69a07aba5b4eea783b8b2ea7dd5e
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2024-03-13 12:11:29 +0530

    Add: Start Enforce 2fa, On mu-plugin load and user logged-in

--- mu-plugins/enforce-2fa.php
+++ mu-plugins/enforce-2fa.php
@@ -16,3 +16,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Do not allow API requests from 2fa users.
 add_filter( 'two_factor_user_api_login_enable', '__return_false', 1 ); // Hook in early to allow overrides.
+
+/**
+ * Force 2FA.
+ */
+function e2fa_enforce_two_factor_plugin() {
+
+	// Check if user is logged in.
+	if ( is_user_logged_in() ) {
+
+		$cap     = apply_filters( 'two_factor_enforcement_cap', 'manage_options' );
+		$limited = current_user_can( $cap );
+	}
+}
+
+// If we're not installing, enable the two-factor plugin on muplugins_loaded.
+if ( ! defined( 'WP_INSTALLING' ) ) {
+
+	// Enable the two-factor plugin after all mu-plugins have been loaded.
+	add_action( 'muplugins_loaded', 'e2fa_enable_two_factor_plugin' );
+}
