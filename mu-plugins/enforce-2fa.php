8791b906239d62324728d426d83241a379cd0bee
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2024-03-13 11:59:50 +0530

    Add: Enforce 2FA Plugin Header

---
+++ mu-plugins/enforce-2fa.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Enforce 2FA
+ * Description: This is mu-plugin to enforce every user to enable 2FA
+ * Author:      Shub
+ * License:     GNU General Public License v3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package Enforce_2FA
+ */
